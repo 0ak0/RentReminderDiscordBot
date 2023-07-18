3f1cf5cc18eef9ec08fcfda688f47751aaa1fff8
0ak0 <32748563+0ak0@users.noreply.github.com>
2023-07-18 22:25:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RentReminderBot
+
+A silly discord bot that reminds me and my friends when we have to pay rent. We named it Helen.
+The rest is our own silly additions
+
+
+Usage requires your bot token in a file next to `main.py` called `token` (Highly reccomended to add this file to a `.gitignore`)
+You must also put a channel id in a file next to `main.py` called `channel` (Highly reccomended to add this file to a `.gitignore`)
+
+You may also want to double check what `daysBeforeWarn` is set to. I like to use 5 as a default.
---
+++ main.py
@@ -0,0 +1,126 @@
+import discord
+from discord.ext import tasks, commands
+import datetime
+from datetime import time, date, datetime
+import calendar
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+description = "Helen Bot - https://github.com/0ak0/RentReminderBot"
+
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="h!", intents=intents, description=description)
+bootDay = datetime.now()
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot Online! Logged in as {bot.user} on " + bootDay.strftime("%H:%M:%S - %A %d %B (%m) %Y"))
+    with open("daysBeforeWarn") as x:
+        warnDays = x.read()
+        x.close
+    print("\ndaysBeforeWarn has been set to " + warnDays)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="h!help"))
+    await bot.add_cog(dateChecker(bot))
+
+@bot.command()
+async def days(ctx):
+    today = datetime.now()
+    daysInMonth = calendar.monthrange(today.year, today.month)[1]
+    daysLeft = daysInMonth - today.day
+
+    await ctx.send("Rent is due in " + str(daysLeft) + " days.")
+
+@bot.command()
+async def warn(ctx, *args):
+    if len(args) == 1:
+        if int(args[0]) > 0 and int(args[0]) < 31:
+            with open("daysBeforeWarn", "r+") as x:
+                x.seek(0)
+                x.write(args[0])
+                x.truncate()
+                x.close
+            with open ("daysBeforeWarn") as x:
+                warnDays = x.read()
+            print("\ndaysBeforeWarn has been set to " + warnDays)
+            await ctx.send("I will now warn you " + warnDays + " days before rent is due.")
+        elif int(args[0]) == 0:
+            with open("daysBeforeWarn", "r+") as x:
+                x.seek(0)
+                x.write("-999999")
+                warnDays = x.read()
+                x.close
+            await ctx.send("Disabled warning.")
+        else:
+            with open("daysBeforeWarn") as x:
+                warnDays = x.read()
+                x.close
+            await ctx.send("I don't understand. Warning you " + warnDays + " days before rent is due.")
+    else:
+        with open("daysBeforeWarn") as x:
+            warnDays = x.read()
+            x.close
+        await ctx.send("I will warn you " + warnDays + " days before rent is due.")
+
+@bot.command()
+async def git(ctx):
+     ctx.send("Make a pr on https://github.com/0ak0/RentReminderBot")
+
+class dateChecker(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.timeCheck.start()
+        print("Date Checker Cog Started!")
+        
+
+    def cog_unload(self):
+        self.timeCheck.cancel()
+
+    @tasks.loop(hours=1)
+    async def timeCheck(self):
+        with open("channel") as x:
+            chStr = x.read()
+            x.close
+        # with open("channel2") as x:
+        #     ch2Str = x.read()
+        #     x.close
+        channel = bot.get_channel(int(chStr)) # This should be set to the id of #gamer-house in The Boys
+        # channel2 = bot.get_channel(int(ch2Str)) # This should be set to the id of #general in test2
+        today = datetime.now()
+        with open("daysBeforeWarn") as x:
+            warnDays = x.read()
+            x.close
+        dayToWarn = calendar.monthrange(today.year, today.month)[1] - int(warnDays)
+        dayToWarnInt = int(dayToWarn)
+        print("\nRunning Loop")
+        todayStr = str(today.day)
+        todayInt = int(today.day)
+        print("Today is: " + todayStr)
+        print("Warn Check is set to " + str(warnDays) + " days before the 1st.")
+        print("Warning on: " + str(dayToWarn))
+        if todayInt == 19:
+                print("Check Success!")
+                print("It works. NOW REMOVE THIS SO YOU DONT GET PINGED AGAIN")
+                mentionOak = "<@437331685231820812>"
+                await channel.send(f"IT WORKS {mentionOak}")  
+        elif todayInt == 1:
+                print("Check Success!")
+                print("Rent Check")
+                mentionBoys = "<@1030961835588984922>"
+                await channel.send(f"# RENT DUE - TODAY IS THE FIRST OF THE MONTH! {mentionBoys}")
+        elif todayInt == dayToWarnInt:
+                print("Check Success!")
+                print("Warn Check")
+                mentionBoys = "<@1030961835588984922>"
+                await channel.send(f"### RENT REMINDER - " + str(warnDays) + " DAYS LEFT {mentionBoys}")
+        else:
+                print("No need to send a message.")
+
+
+with open("token") as x:
+    tokenStr = x.read()
+    x.close
+bot.run(tokenStr)
